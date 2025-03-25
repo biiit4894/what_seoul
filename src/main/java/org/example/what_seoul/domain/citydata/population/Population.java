556e4bd4ac643c5c0b49b0a0acd314c186a742da
biiit4894 <ghiesf@naver.com>
2025-03-25 17:40:09 +0900

    :art: 엔터티 NoArgsConstructor AccessLevel.PROTECTED 제거
    - 원래 이걸 쓰는 이유
      1. JPA에서 프록시 생성을 위해 기본 생성자 필요
      2. 기본 생성자를 외부에서 직접 호출하지 못하도록 보호하고 싶을 때 사용
      3. 정적 팩토리 메소드나 명시적인 생성자 호출을 통해 객체를 만들도록 강제할 때 유용함
    
     - But 지금 프로젝트는
       1. 조회한 데이터를 가공하지 않고 그대로 저장함
       2. 엔터티의 상태를 변경할 비즈니스 로직이 아직 거의 없음
         ex) 특정 조건을 만족해야만 엔터티 생성이 가능하고, 엔터티 내부에서 유효성 검사를 진행하는 경우
       3. 스케줄러에서 직접 엔터티를 생성해서 저장함
       => 생성자를 직접 호출해서 객체를 만들어도 문제 없을 것이라고 판단함.

--- src/main/java/org/example/what_seoul/domain/citydata/population/Population.java
+++ src/main/java/org/example/what_seoul/domain/citydata/population/Population.java
@@ -14,7 +14,7 @@ import java.util.List;
  */
 @Entity
 @Getter
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor
 public class Population {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
--- src/main/java/org/example/what_seoul/domain/citydata/population/PopulationForecast.java
+++ src/main/java/org/example/what_seoul/domain/citydata/population/PopulationForecast.java
@@ -10,7 +10,7 @@ import lombok.NoArgsConstructor;
  */
 @Entity
 @Getter
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor
 public class PopulationForecast {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
