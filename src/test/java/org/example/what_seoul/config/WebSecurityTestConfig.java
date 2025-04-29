62c2ab0f458fb4b3ddd89eca72a91a005388808c
biiit4894 <ghiesf@naver.com>
2025-04-29 14:53:38 +0900

    :passport_control: 유저 권한 ADMIN, USER로 분리하여 관리

--- src/test/java/org/example/what_seoul/config/WebSecurityTestConfig.java
+++ src/test/java/org/example/what_seoul/config/WebSecurityTestConfig.java
@@ -2,6 +2,7 @@ package org.example.what_seoul.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -12,10 +13,12 @@ public class WebSecurityTestConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(auth ->
-                auth.requestMatchers("/api/user/signup", "/api/user/{id}").permitAll()
-                        .anyRequest()
-                        .authenticated()
+        http.authorizeHttpRequests(auth -> auth
+                .requestMatchers("/api/area/all", "/api/area/all/weather", "/api/area/all/ppltn", "/", "/login", "/signup", "/api/user/signup", "/api/user/{id}").permitAll()
+                .requestMatchers(HttpMethod.GET, "/api/user/list").hasRole("ADMIN")
+                .requestMatchers(HttpMethod.GET, "/api/user/{id}").hasAnyRole("ADMIN", "USER")
+                .requestMatchers("/api/user/update", "/api/user/withdraw").authenticated()
+                .anyRequest().denyAll()
         ).csrf(auth -> auth.disable());
         return http.build();
     }
