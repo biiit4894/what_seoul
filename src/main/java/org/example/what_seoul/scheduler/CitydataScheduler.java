a6055ad6aca54550bee0d6a0db7b44668d53aece
biiit4894 <ghiesf@naver.com>
2025-05-15 20:19:08 +0900

    :recycle: 도시데이터 저장 스케줄러 리팩토링[7]
    1. call() 메소드의 책임 분리 (fetchCityData, fetchCityDataSync, getDocument 메소드 분리)
      - 필요했던 이유?
        - 동기/비동기 로직, 외부 요청, 데이터파싱이 call()로부터 시작되는 한 클래스에 혼재되어 있었음
      - 효과?
        - 1. 메소드마다 하나의 일만 하도록 하자. (단일책임원칙 준수)
        - 2. fetchCityDataSync, getDocument 분리로 인해 단위 테스트 작성 용이
    
    2. 도시데이터 파싱 메소드들을 CitydataParser 클래스에 따로 분리
      - 필요했던 이유?
        - 데이터 파싱 로직이 스케줄러에 존재하면서 스케줄러가 비즈니스 로직과 파싱 책임을 모두 가지고 있었음.
        - 그런데 사실 파싱 자체는 외부 상태에 크게 의존하지 않고 독립적으로 분리하기가 오히려 편해 보인다. (Document, Area만 있으면 항상 같은 결과를 반환한다.)
      - 효과?
        - 1. 스케줄러는 말 그대로 스케줄링과 흐름 제어를 담당하고, 파서 클래스는 필요한 데이터를 추출하고 가공하는 역할만 담당 (관심사 분리)
        - 2. 단위 테스트 작성 용이 (파서 메소드 인자에는 크게 Document 객체, Area 객체만 들어가도 된다)

--- src/main/java/org/example/what_seoul/scheduler/CitydataScheduler.java
+++ src/main/java/org/example/what_seoul/scheduler/CitydataScheduler.java
@@ -14,15 +14,14 @@ import org.example.what_seoul.repository.citydata.event.CultureEventRepository;
 import org.example.what_seoul.repository.citydata.population.PopulationForecastRepository;
 import org.example.what_seoul.repository.citydata.population.PopulationRepository;
 import org.example.what_seoul.repository.citydata.weather.WeatherRepository;
+import org.example.what_seoul.service.citydata.CitydataParser;
 import org.example.what_seoul.service.citydata.CitydataService;
-import org.example.what_seoul.service.citydata.PcpMsgHistoryService;
-import org.example.what_seoul.util.XmlElementNames;
+//import org.example.what_seoul.service.citydata.PcpMsgHistoryService;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.w3c.dom.Document;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
+
 import org.xml.sax.SAXException;
 
 import javax.xml.parsers.DocumentBuilder;
@@ -41,13 +40,14 @@ import java.util.stream.Collectors;
 @Slf4j
 public class CitydataScheduler {
     private final CitydataService citydataService;
+    private final CitydataParser citydataParser;
     private final AreaRepository areaRepository;
     private final PopulationRepository populationRepository;
     private final PopulationForecastRepository populationForecastRepository;
     private final WeatherRepository weatherRepository;
     private final CultureEventRepository cultureEventRepository;
 
-    private final PcpMsgHistoryService pcpMsgHistoryService;
+//    private final PcpMsgHistoryService pcpMsgHistoryService;
 
     @Value("${seoul.open.api.url}")
     private String url;
@@ -110,244 +110,63 @@ public class CitydataScheduler {
 
 
     /**
-     * 특정 장소의 도시 데이터를 공공데이터 API에서 가져와 XML Document로 반환
-     *
-     * @param area
-     * @return
+     * 특정 지역에 대해 비동기로 도시 데이터를 조회하는 메소드.
+     * 내부적으로 fetchCityDataSync를 별도 스레드에서 실행한다.
+     * @param area                          도시데이터를 조회할 Area 정보
+     * @param isUpdateCultureEventHour      문화행사 데이터 갱신 여부 플래그
+     * @return CompletableFuture<CityData>  비동기 처리 결과를 포함하는 Future 객체
      */
     CompletableFuture<CityData> fetchCityData(Area area, boolean isUpdateCultureEventHour) {
-        return CompletableFuture.supplyAsync(() -> {
-            try {
-                String sanitizedAreaName = area.getAreaName().replace("&", "&amp;");
-                String encodedAreaName = URLEncoder.encode(sanitizedAreaName, StandardCharsets.UTF_8);
-                log.info("sanitized area name: {}, id: {}", sanitizedAreaName, area.getId());
-                log.info("encoded area name: {}", encodedAreaName);
-
-                DocumentBuilder documentBuilder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
-                Document document = documentBuilder.parse(url + encodedAreaName);
-                document.getDocumentElement().normalize();
-
-                Population population = parsePopulationData(document, area);
-                List<PopulationForecast> populationForecast = parsePopulationForecastData(document, population, area);
-                Weather weather = parseWeatherData(document, area);
-
-                List<CultureEvent> cultureEvent = isUpdateCultureEventHour ? parseCultureEventData(document, area) : Collections.emptyList();
-
-                return new CityData(population, populationForecast, weather, cultureEvent);
-            } catch (ParserConfigurationException | IOException | SAXException e) {
-                log.error("failed to fetch city data for area {}: {}", area.getAreaName(), e.getMessage());
-                throw new CitydataSchedulerException("도시 데이터 fetch에 실패했습니다.");
-            }
-        });
+        return CompletableFuture.supplyAsync(() -> fetchCityDataSync(area, isUpdateCultureEventHour));
 
     }
 
     /**
-     * XML 문서에서 인구 데이터를 파싱하여 Population 객체 생성
-     * @param document
-     * @param area
-     * @return
-     */
-    private Population parsePopulationData(Document document, Area area) {
-        try {
-            NodeList populationNodeList = document.getElementsByTagName(XmlElementNames.LIVE_PPLTN_STTS.getXmlElementName());
-
-            String congestionLevel = "No Tag";
-            String congestionMessage = "No Tag";
-            String minPopulation = "No Tag";
-            String maxPopulation = "No Tag";
-            String populationUpdateTime = "No Tag";
-
-            if (populationNodeList != null && populationNodeList.getLength() > 0) {
-                Node populationParentNode = populationNodeList.item(1);
-                if (populationParentNode == null) {
-                    log.warn("population parent node is null for area: {}", area.getAreaName());
-                } else {
-                    NodeList childNodes = populationParentNode.getChildNodes();
-
-                    for (int i = 0; i < childNodes.getLength(); i++) {
-                        Node childNode = childNodes.item(i);
-                        if (childNode.getNodeName().equals(XmlElementNames.AREA_CONGEST_LVL.getXmlElementName())) {
-                            congestionLevel = childNode.getTextContent();
-                        } else if (childNode.getNodeName().equals(XmlElementNames.AREA_CONGEST_MSG.getXmlElementName())) {
-                            congestionMessage = childNode.getTextContent();
-                        } else if (childNode.getNodeName().equals(XmlElementNames.AREA_PPLTN_MIN.getXmlElementName())) {
-                            minPopulation = childNode.getTextContent();
-                        } else if (childNode.getNodeName().equals(XmlElementNames.AREA_PPLTN_MAX.getXmlElementName())) {
-                            maxPopulation = childNode.getTextContent();
-                        } else if (childNode.getNodeName().equals(XmlElementNames.PPLTN_TIME.getXmlElementName())) {
-                            populationUpdateTime = childNode.getTextContent();
-                        }
-                    }
-                }
-            }
-            return new Population(
-                    congestionLevel, congestionMessage, minPopulation, maxPopulation, populationUpdateTime, area
-            );
-        } catch (Exception e) {
-            log.error("failed to parse city data for area {}: {}", area.getAreaName(), e.getMessage());
-            throw new CitydataSchedulerException(area.getAreaName() + "의 인구 현황 데이터 파싱에 실패했습니다.");
-        }
-
-    }
-
-    /**
-     * XML 문서에서 인구 예측 데이터를 파싱하여 PopulationForecast 객체 리스트 생성
+     * 특정 장소에 대해 동기적으로 도시 데이터를 조회하는 메소드.
+     * 공공데이터 API에서 XML Document를 가져오고, 이를 Population, PopulationForecast, Weather, CultureEvent로 파싷애 반환한다.
      *
-     * @param document
-     * @param population
-     * @param area
-     * @return
+     * @param area                        조회할 장소 정보
+     * @param isUpdateCultureEventHour    문화행사 데이터 갱신 여부 플래그
+     * @return CityData                   조회된 도시 데이터 객체
+     * @throws CitydataSchedulerException XML 파싱이나 네트워크 요청 실패 시 발생
      */
-    private List<PopulationForecast> parsePopulationForecastData(Document document, Population population, Area area) {
+    public CityData fetchCityDataSync(Area area, boolean isUpdateCultureEventHour) {
         try {
-            List<PopulationForecast> forecastList = new ArrayList<>();
-            NodeList nodeList = document.getElementsByTagName(XmlElementNames.FCST_PPLTN.getXmlElementName());
+            Document document = getDocument(area);
 
-            String forecastCongestionLevel = "No Tag";
-            String forecastPpltnMin = "No Tag";
-            String forecastPpltnMax = "No Tag";
-            String forecasePplTime = "No Tag";
+            Population population = citydataParser.parsePopulationData(document, area);
+            List<PopulationForecast> populationForecast = citydataParser.parsePopulationForecastData(document, population, area);
+            Weather weather = citydataParser.parseWeatherData(document, area);
 
-            if (nodeList == null || nodeList.getLength() == 0) {
-                log.warn("No population forecast data found for area: {}", population.getArea().getAreaName());
-            }
-            for (int i = 1; i < nodeList.getLength(); i++) {
-                Node fcstPpltnNode = nodeList.item(i);
-                NodeList childNodes = fcstPpltnNode.getChildNodes();
+            List<CultureEvent> cultureEvent = isUpdateCultureEventHour ? citydataParser.parseCultureEventData(document, area) : Collections.emptyList();
 
-                for (int j = 0; j < childNodes.getLength(); j++) {
-                    Node childNode = childNodes.item(j);
-                    if (childNode.getNodeName().equals(XmlElementNames.FCST_CONGEST_LVL.getXmlElementName())) {
-                        forecastCongestionLevel = childNode.getTextContent();
-                    } else if (childNode.getNodeName().equals(XmlElementNames.FCST_PPLTN_MIN.getXmlElementName())) {
-                        forecastPpltnMin = childNode.getTextContent();
-                    } else if (childNode.getNodeName().equals(XmlElementNames.FCST_PPLTN_MAX.getXmlElementName())) {
-                        forecastPpltnMax = childNode.getTextContent();
-                    } else if (childNode.getNodeName().equals(XmlElementNames.FCST_TIME.getXmlElementName())) {
-                        forecasePplTime = childNode.getTextContent();
-                    }
-                }
-
-                forecastList.add(new PopulationForecast(
-                        forecastCongestionLevel,
-                        forecastPpltnMin,
-                        forecastPpltnMax,
-                        forecasePplTime,
-                        population));
-
-            }
-            return forecastList;
-        } catch(Exception e) {
-            log.error("failed to parse city data for area {}: {}", area.getAreaName(), e.getMessage());
-            throw new CitydataSchedulerException(area.getAreaName() + "의 인구 예측값 데이터 파싱에 실패했습니다.");
+            return new CityData(population, populationForecast, weather, cultureEvent);
+        } catch (ParserConfigurationException | IOException | SAXException e) {
+            log.error("failed to fetch city data for area {}: {}", area.getAreaName(), e.getMessage());
+            throw new CitydataSchedulerException("도시 데이터 fetch에 실패했습니다.");
         }
     }
 
     /**
-     * XML 문서에서 날씨 데이터를 파싱하여 PopulationForecast 객체 리스트 생성
+     * 특정 장소의 공공데이터 API URL로부터 XML Document를 가져오는 메소드.
+     * 지역 이름에 포함된 '&' 문자를 안전하게 치환하고 URL 인코딩 후 요청한다.
      *
-     * @param document
-     * @param area
-     * @return
+     * @param area 조회할 장소 정보
+     * @return Document 요청한 XML 문서 객체
+     * @throws ParserConfigurationException 문서 빌더 구성 오류 발생 시
+     * @throws SAXException XML 파싱 오류 발생 시
+     * @throws IOException 네트워크 I/O 오류 발생 시
      */
-    private Weather parseWeatherData(Document document, Area area) {
-        try {
-            // 날씨 데이터를 저장할 Map
-            Map<XmlElementNames, String> weatherData = new HashMap<>();
-            // 날씨 데이터를 담고 있는 XML 노드들 조회
-            NodeList weatherNodeList = document.getElementsByTagName(XmlElementNames.WEATHER_STTS.getXmlElementName());
-            // 인덱스 1의 노드를 가져오기 (최상위 노드의 하위 노드에 필요한 노드들이 위치함)
-            Node weatherParentNode = weatherNodeList.item(1);
-            if (weatherParentNode == null) {
-                log.warn("weather parent node is null for area: {}", area.getAreaName());
-            } else {
-                // 자식 노드들에 대해 반복하며 날씨 정보를 Map에 저장
-                NodeList childNodeList = weatherParentNode.getChildNodes();
-                for (int i = 0; i < childNodeList.getLength(); i++) {
-                    Node childNode = childNodeList.item(i);
+    protected Document getDocument(Area area) throws ParserConfigurationException, SAXException, IOException {
+        log.info("getDocument called with area: " + area);
+        String sanitizedAreaName = area.getAreaName().replace("&", "&amp;");
+        String encodedAreaName = URLEncoder.encode(sanitizedAreaName, StandardCharsets.UTF_8);
+        log.info("sanitized area name: {}, id: {}", sanitizedAreaName, area.getId());
+        log.info("encoded area name: {}", encodedAreaName);
 
-                    // 각 자식 노드의 이름이 enum에 정의된 이름과 일치하면,
-                    // 그 노드를 Map의 key로, 노드의 텍스트 내용을 value로 저장
-                    for (XmlElementNames element : XmlElementNames.values()) {
-                        if (childNode.getNodeName().equals(element.getXmlElementName())) {
-                            weatherData.put(element, childNode.getTextContent());
-                            break;
-                        }
-                    }
-                }
-            }
-
-            // PCP_MSG 값 추출
-            String pcpMsg = weatherData.getOrDefault(XmlElementNames.PCP_MSG, "No Tag");
-
-            // 히스토리 저장
-            pcpMsgHistoryService.saveIfNotExists(pcpMsg);
-
-            // Map에서 주어진 key(XmlElementNames)에 해당하는 값을 가져오고, 만약 해당 key가 없다면 "No Tag"를 기본값으로 반환
-            return new Weather(
-                    weatherData.getOrDefault(XmlElementNames.TEMP, "No Tag"),
-                    weatherData.getOrDefault(XmlElementNames.MAX_TEMP, "No Tag"),
-                    weatherData.getOrDefault(XmlElementNames.MIN_TEMP, "No Tag"),
-                    weatherData.getOrDefault(XmlElementNames.PM25_INDEX, "No Tag"),
-                    weatherData.getOrDefault(XmlElementNames.PM25, "No Tag"),
-                    weatherData.getOrDefault(XmlElementNames.PM10_INDEX, "No Tag"),
-                    weatherData.getOrDefault(XmlElementNames.PM10, "No Tag"),
-                    weatherData.getOrDefault(XmlElementNames.PCP_MSG, "No Tag"),
-                    weatherData.getOrDefault(XmlElementNames.WEATHER_TIME, "No Tag"),
-                    area
-            );
-        } catch (Exception e) {
-            log.error("failed to parse city data for area {}: {}", area.getAreaName(), e.getMessage());
-            throw new CitydataSchedulerException(area.getAreaName() + "의 날씨 현황 데이터 파싱에 실패했습니다.");
-        }
-
+        DocumentBuilder documentBuilder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+        Document document = documentBuilder.parse(url + encodedAreaName);
+        document.getDocumentElement().normalize();
+        return document;
     }
-
-    private List<CultureEvent> parseCultureEventData(Document document, Area area) {
-        try {
-            List<CultureEvent> cultureEventList = new ArrayList<>();
-            NodeList cultureEventNodeList = document.getElementsByTagName(XmlElementNames.EVENT_STTS.getXmlElementName());
-            String eventNm = "No Tag";
-            String eventPeriod = "No Tag";
-            String eventPlace = "No Tag";
-            String eventX = "No Tag";
-            String eventY = "No Tag";
-            String thumbnail = "No Tag";
-            String url = "No Tag";
-
-            if (cultureEventNodeList != null && cultureEventNodeList.getLength() != 0) {
-                for (int i = 1; i < cultureEventNodeList.getLength(); i++) {
-                    Node cultureEventNode = cultureEventNodeList.item(i);
-                    NodeList childNodes = cultureEventNode.getChildNodes();
-
-                    for (int j = 0; j < childNodes.getLength(); j++) {
-                        Node childNode = childNodes.item(j);
-                        if (childNode.getNodeName().equals(XmlElementNames.EVENT_NM.getXmlElementName())) {
-                            eventNm = childNode.getTextContent();
-                        } else if (childNode.getNodeName().equals(XmlElementNames.EVENT_PERIOD.getXmlElementName())) {
-                            eventPeriod = childNode.getTextContent();
-                        } else if (childNode.getNodeName().equals(XmlElementNames.EVENT_PLACE.getXmlElementName())) {
-                            eventPlace = childNode.getTextContent();
-                        } else if (childNode.getNodeName().equals(XmlElementNames.EVENT_X.getXmlElementName())) {
-                            eventX = childNode.getTextContent();
-                        } else if (childNode.getNodeName().equals(XmlElementNames.EVENT_Y.getXmlElementName())) {
-                            eventY = childNode.getTextContent();
-                        } else if (childNode.getNodeName().equals(XmlElementNames.THUMBNAIL.getXmlElementName())) {
-                            thumbnail = childNode.getTextContent();
-                        } else if (childNode.getNodeName().equals(XmlElementNames.URL.getXmlElementName())) {
-                            url = childNode.getTextContent();
-                        }
-                    }
-                    cultureEventList.add(new CultureEvent(eventNm, eventPeriod, eventPlace, eventX, eventY, thumbnail, url, area));
-                }
-            }
-            return cultureEventList;
-        } catch (Exception e) {
-            log.error("failed to parse city data for area {}: {}", area.getAreaName(), e.getMessage());
-            throw new CitydataSchedulerException(area.getAreaName() + "의 문화행사 데이터 파싱에 실패했습니다.");
-        }
-
-    }
-
 }
