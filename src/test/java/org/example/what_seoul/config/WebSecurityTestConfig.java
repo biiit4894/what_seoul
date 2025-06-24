456ed1cfaa077b3e21ef426e4e87f3d51a86ef28
biiit4894 <ghiesf@naver.com>
2025-06-25 03:59:19 +0900

    :construction_worker: 테스트 환경을 위한 security config에 TestConfiguration 애노테이션 추가

--- src/test/java/org/example/what_seoul/config/WebSecurityTestConfig.java
+++ src/test/java/org/example/what_seoul/config/WebSecurityTestConfig.java
@@ -1,18 +1,16 @@
 package org.example.what_seoul.config;
 
+import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
-import static org.springframework.boot.autoconfigure.security.servlet.PathRequest.toH2Console;
 
-@Configuration
+@TestConfiguration
 @EnableWebSecurity
 @Profile("test")
 public class WebSecurityTestConfig {
