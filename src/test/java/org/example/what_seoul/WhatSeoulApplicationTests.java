b8cf49c32437b2f3625f5665cf0dc5656880a125
biiit4894 <ghiesf@naver.com>
2025-06-25 03:42:54 +0900

    :construction_worker: WhatSeoulApplicationTests 비활성화

--- src/test/java/org/example/what_seoul/WhatSeoulApplicationTests.java
+++ src/test/java/org/example/what_seoul/WhatSeoulApplicationTests.java
@@ -1,9 +1,10 @@
 package org.example.what_seoul;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Disabled;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@Disabled
 class WhatSeoulApplicationTests {
 
 
