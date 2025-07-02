3e01db50cbbf4ef9b667e4d742bdfcbc9c99a1b2
biiit4894 <ghiesf@naver.com>
2025-07-02 17:43:54 +0900

    :lipstick: 관리자용 Settings 페이지 추가
    - 관리자로 로그인한 상태에서 'Settings > 계정 관리 > 관리자 계정 생성' 경로로 접근해야 관리자 계정 생성 가능

---
+++ src/main/java/org/example/what_seoul/controller/admin/AdminViewController.java
@@ -0,0 +1,42 @@
+package org.example.what_seoul.controller.admin;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.example.what_seoul.service.admin.AdminService;
+import org.example.what_seoul.service.user.UserService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Objects;
+
+@Controller
+@RequiredArgsConstructor
+@Slf4j
+public class AdminViewController {
+    private final AdminService adminService;
+    private final UserService userService;
+
+
+    @GetMapping("/settings")
+    public String settings(Model model) {
+        model.addAttribute("authPrincipal", userService.getAuthenticationPrincipal());
+
+        if (!Objects.equals(model.getAttribute("authPrincipal"), "anonymousUser") && userService.getLoginUserInfo() != null) {
+            model.addAttribute("loginUserInfo", userService.getLoginUserInfo());
+        }
+
+        return "/admin/settings";
+    }
+
+    @GetMapping("/new-admin")
+    public String newAdmin(Model model) {
+        model.addAttribute("authPrincipal", userService.getAuthenticationPrincipal());
+
+        if (!Objects.equals(model.getAttribute("authPrincipal"), "anonymousUser") && userService.getLoginUserInfo() != null) {
+            model.addAttribute("loginUserInfo", userService.getLoginUserInfo());
+        }
+
+        return "/admin/new-admin";
+    }
+}
