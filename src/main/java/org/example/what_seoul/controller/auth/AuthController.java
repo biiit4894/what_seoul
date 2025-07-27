a5597c053471168bdff9dbc3e9c1bbdb36a3c975
biiit4894 <ghiesf@naver.com>
2025-07-27 17:11:38 +0900

    :memo: Auth API 에러 응답 애노테이션 사용 및 Operation 애노테이션 Description 속성 값을 클래스로 분리

--- src/main/java/org/example/what_seoul/controller/auth/AuthController.java
+++ src/main/java/org/example/what_seoul/controller/auth/AuthController.java
@@ -1,9 +1,6 @@
 package org.example.what_seoul.controller.auth;
 
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.ExampleObject;
-import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -12,6 +9,8 @@ import lombok.RequiredArgsConstructor;
 import org.example.what_seoul.common.dto.CommonResponse;
 import org.example.what_seoul.controller.auth.dto.ResReissueAccessTokenDTO;
 import org.example.what_seoul.service.auth.AuthService;
+import org.example.what_seoul.swagger.operation.description.auth.AuthDescription;
+import org.example.what_seoul.swagger.responses.error.CommonErrorResponses;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -23,60 +22,20 @@ import org.springframework.web.bind.annotation.*;
 public class AuthController {
     private final AuthService authService;
 
-    @Operation(
-            summary = "액세스 토큰 재발급",
-            description = """
-            Refresh Token을 이용해 Access Token을 재발급합니다.\s
-            - Refresh Token은 쿠키에 담겨 전달되어야 합니다. (refreshToken 쿠키 사용)\s
-            - 재발급된 Access Token은 쿠키에 담아 응답됩니다.
-            """
-    )
+    @Operation(summary = "액세스 토큰 재발급", description = AuthDescription.REISSUE_ACCESS_TOKEN)
+    @CommonErrorResponses
     @ApiResponses({
-            @ApiResponse(responseCode = "200", description = "재발급 성공", content = @Content(
-                    schema = @Schema(implementation = ResReissueAccessTokenDTO.class),
-                    examples = @ExampleObject(
-                            value = """
-                            {
-                              "success": true,
-                              "message": "액세스 토큰 재발급 성공",
-                              "data": {
-                                "accessTokenExpiration": 1721043562000
-                              },
-                              "responseTime": "2025-07-15T12:00:00.000Z"
-                            }
-                            """
-                    )
-            )),
-            @ApiResponse(responseCode = "401", description = "유효하지 않은 Refresh Token")
+            @ApiResponse(responseCode = "200", description = "재발급 성공")
     })
     @PostMapping("/access/reissue")
     public ResponseEntity<CommonResponse<ResReissueAccessTokenDTO>> reissueAccessToken(@CookieValue("refreshToken") String refreshToken, HttpServletResponse response) {
         return ResponseEntity.status(HttpStatus.OK).body(authService.reissueAccessToken(refreshToken, response));
     }
 
-    @Operation(
-            summary = "로그아웃",
-            description = """
-            현재 로그인된 사용자의 토큰을 무효화하고 쿠키에서 제거합니다.
-            - Access Token은 accessToken 쿠키를 통해 전달되어야 합니다.
-            - Redis에 저장된 Refresh Token도 삭제됩니다.
-            - AccessToken, RefreshToken 쿠키 모두 즉시 만료 처리됩니다.
-            """
-    )
+    @Operation(summary = "로그아웃", description = AuthDescription.LOGOUT)
+    @CommonErrorResponses
     @ApiResponses({
-            @ApiResponse(responseCode = "200", description = "로그아웃 성공", content = @Content(
-                    examples = @ExampleObject(
-                            value = """
-                    {
-                      "success": true,
-                      "message": "로그아웃 성공",
-                      "data": null,
-                      "responseTime": "2025-07-15T12:00:00.000Z"
-                    }
-                    """
-                    )
-            )),
-            @ApiResponse(responseCode = "401", description = "유효하지 않거나 만료된 Access Token")
+            @ApiResponse(responseCode = "200", description = "로그아웃 성공")
     })
     @PostMapping("/logout")
     public ResponseEntity<CommonResponse<Void>> logout(@CookieValue("accessToken") String accessToken, HttpServletResponse response) {
---
+++ src/main/java/org/example/what_seoul/swagger/operation/description/auth/AuthDescription.java
@@ -0,0 +1,16 @@
+package org.example.what_seoul.swagger.operation.description.auth;
+
+public class AuthDescription {
+    public static final String REISSUE_ACCESS_TOKEN = """
+            Refresh Token을 이용해 Access Token을 재발급합니다.\s
+            - Refresh Token은 쿠키에 담겨 전달되어야 합니다. (refreshToken 쿠키 사용)\s
+            - 재발급된 Access Token은 쿠키에 담아 응답됩니다.
+            """;
+
+    public static final String LOGOUT = """
+            현재 로그인된 사용자의 토큰을 무효화하고 쿠키에서 제거합니다.
+            - Access Token은 accessToken 쿠키를 통해 전달되어야 합니다.
+            - Redis에 저장된 Refresh Token도 삭제됩니다.
+            - AccessToken, RefreshToken 쿠키 모두 즉시 만료 처리됩니다.
+            """;
+}
